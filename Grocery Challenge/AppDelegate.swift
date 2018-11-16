6c60c8c2f1db7c9789fcd4fab06e6ff33f277080
Andrew Crookston <andrew.crookston@instacart.com>
2018-11-15 16:27:21 -0800

    Update Swift to 4.2

--- Grocery Challenge/AppDelegate.swift
+++ Grocery Challenge/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
         // If you don't want to use storyboards, here's something to get you started.
